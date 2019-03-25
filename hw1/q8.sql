e91c114091d8f4482a0e772be965dcd08d7ad09c
casper <cpr1014@gmail.com>
2019-03-25 11:11:06 +0800

    bug

--- hw1/q8.sql
+++ hw1/q8.sql
@@ -2,9 +2,8 @@ SELECT teamRoadKills, AVG(teamPlace) AS avgWinPlacePerc
 FROM
   (
   SELECT SUM(player_statistic.roadKills) AS teamRoadKills, AVG(player_statistic.winPlacePerc) AS teamPlace
-  FROM `player_statistic`
-  INNER JOIN `match`
-  ON player_statistic.matchId=match.matchId
+  FROM `player_statistic`, `match`
+  WHERE player_statistic.matchId=match.matchId AND (match.matchType='squad' OR match.matchType='squad-fpp')
   GROUP BY player_statistic.groupId
   ) teamInfo
 GROUP BY teamRoadKills
